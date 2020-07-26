878b3aa60ab86981bfb558025cd738d4d71d3254
Ismail F Aziz <25928284+skykykykykykykykykykykys@users.noreply.github.com>
2020-07-26 21:38:23 +0700

    added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // require the discord.js module
 const Discord = require('discord.js');
-const config = require('../config.json');
+const { token, prefix } = require('../config.json');
 const lyrics = require('./lyrics');
 
 // create a new Discord client
@@ -14,23 +14,33 @@ client.once('ready', () => {
 
 //listener
 
-client.on('message', message => {
-    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-    const args = message.content.slice(config.prefix.length).trim().split(' ');
+client.on('message', async message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).trim().split(' ');
     const command = args.shift().toLowerCase();
     console.log(message.content)
 
 	if (command === 'ping') {
         // send back "Pong." to the channel the message was sent in
         message.channel.send('Pong.')
-    } else if (message.content === '!weeb') {
-        console.log(message.content)
-        console.log(lyrics.getLyrics("one ok rock", "the beginning"))
-        message.channel.send("test")
+        
+    } else if (command === 'lyrics') {
+        let songTitle = args.slice(1)
+        let songAuthor =  args[0]
+
+        let lirik = await lyrics.getLyrics(songAuthor, songTitle)
+        message.channel.send(lirik)
+        
+    } else if (command === 'argsinfo') {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+    
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
     }
 });
 
 
 
 // login to Discord with your app's token
-client.login(config.token);
+client.login(token);
--- src/lyrics.js
+++ src/lyrics.js
@@ -3,7 +3,6 @@ const lyricsFinder = require('lyrics-finder');
 async function getLyrics(artist, title) {
 
     let lyrics = await lyricsFinder(artist, title) || "Not Found!";
-    console.log(lyrics);
     return lyrics;
     
 }
