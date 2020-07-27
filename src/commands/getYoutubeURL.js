9030f201c463f0b4ab244634fd09dfbc85acc01f
ARadhitama <ariqradhitamaa@gmail.com>
2020-07-27 12:49:32 +0700

    semangat ismail

--- src/commands/getYoutubeURL.js
+++ src/commands/getYoutubeURL.js
@@ -2,7 +2,6 @@ const { Command } = require('discord-akairo');
 const { GOOGLE_API } = require('../../config.json')
 
 const lyrics = require('./lyrics');
-const music = require('./music')
 
 const axios = require('axios');
 const querystring = require('querystring')
@@ -63,4 +62,47 @@ class SearchCommand extends Command {
     }
 }
 
-module.exports = SearchCommand;
+async function getYoutubeURL(message, args) {
+    const params = {
+        q: args,
+        part: 'id',
+        maxResults: 30,
+        key: GOOGLE_API,
+    }
+
+    await axios.get('https://www.googleapis.com/youtube/v3/search?' + querystring.stringify(params))
+        .then(function(response) {
+            const result = response.data
+            const findings = result.items.map(function(item) {
+                let link = ''
+                let id = ''
+                switch (item.id.kind) {
+                    case 'youtube#channel':
+                        link = 'https://www.youtube.com/channel/' + item.id.channelId
+                        id = item.id.channelId
+                        break
+                    case 'youtube#playlist':
+                        link = 'https://www.youtube.com/playlist?list=' + item.id.playlistId
+                        id = item.id.playlistId
+                        break
+                    default:
+                        link = 'https://www.youtube.com/watch?v=' + item.id.videoId
+                        id = item.id.videoId
+                        break
+                }
+
+                return {
+                    id: id,
+                    link: link,
+                    kind: item.id.kind,
+                }
+            })
+            console.log(findings)
+            return message.reply('nih linknya bang', findings[0].link)
+        })
+    .catch(function(err) {
+        return console.log(err)
+    })
+}
+
+module.exports = SearchCommand, getYoutubeURL;
--- src/commands/music.js
+++ src/commands/music.js
@@ -1,4 +1,8 @@
 const ytdl = require('ytdl-core');
+const { Command } = require('discord-akairo');
+
+const lyrics = require('./lyrics');
+const youtubeUrl = require('./getYoutubeURL');
 
 async function execute(message, serverQueue) {
 	const args = message.content.split(' ');
@@ -50,8 +54,66 @@ async function execute(message, serverQueue) {
 
 }
 
+class ExecuteMusic extends Command {
+    constructor() {
+        super('play', {
+            aliases: ['play'],
+        });
+    }
+
+    async exec(message, serverQueue) {
+		const args = message.content.split(' ');
+		const songTitle = args.slice(1)
+        const songAuthor = args[0]
+
+		const voiceChannel = message.member.voice.channel;
+		if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
+		const permissions = voiceChannel.permissionsFor(message.client.user);
+		if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+			return message.channel.send('I need the permissions to join and speak in your voice channel!');
+		}
+
+		const songInfo = await ytdl.getInfo(args[1]);
+		const song = {
+			title: songInfo.title,
+			url: songInfo.video_url,
+		};
+
+		if (!serverQueue) {
+			const queueContruct = {
+				textChannel: message.channel,
+				voiceChannel: voiceChannel,
+				connection: null,
+				songs: [],
+				volume: 5,
+				playing: true,
+			};
+
+			// eslint-disable-next-line no-undef
+			queue.set(message.guild.id, queueContruct);
+
+			queueContruct.songs.push(song);
+
+			try {
+				const connection = await voiceChannel.join();
+				queueContruct.connection = connection;
+				play(message.guild, queueContruct.songs[0]);
+			}
+			catch (err) {
+				console.log(err);
+				queue.delete(message.guild.id);
+				return message.channel.send(err);
+			}
+		}
+		else {
+			serverQueue.songs.push(song);
+			console.log(serverQueue.songs);
+			return message.channel.send(`${song.title} has been added to the queue!`);
+		}
+	}
+}
 
 module.exports = {
-    execute,
+    ExecuteMusic,
 }
 
