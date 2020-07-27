f241beae847463cc7754c1b899cecf3d16921240
ARadhitama <ariqradhitamaa@gmail.com>
2020-07-27 08:14:31 +0700

    add music

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@
 const Discord = require('discord.js');
 const { token, prefix } = require('../config.json');
 const lyrics = require('./lyrics');
+const ytdl = require('ytdl-core');
+const music = require('./music')
 
 // create a new Discord client
 const client = new Discord.Client();
+const queue = new Map();
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
@@ -19,6 +22,7 @@ client.on('message', async message => {
     const args = message.content.slice(prefix.length).trim().split(' ');
     const command = args.shift().toLowerCase();
     console.log(message.content)
+    const serverQueue = queue.get(message.guild.id);
 
 	if (command === 'ping') {
         // send back "Pong." to the channel the message was sent in
@@ -35,7 +39,18 @@ client.on('message', async message => {
         if (!args.length) {
             return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
         }
-    
+    } else if (command === 'play') {
+        lagu = music.execute(message, serverQueue);
+        message.channel.send(lagu);
+    // } else if (command === 'skip') {
+    //         skip(message, serverQueue);
+    //         return;
+    // } else if (command === 'stop') {
+    //         stop(message, serverQueue);
+    //         return;
+    // } else {
+    //         message.channel.send('You need to enter a valid command!')
+    // }   
         message.channel.send(`Command name: ${command}\nArguments: ${args}`);
     }
 });
